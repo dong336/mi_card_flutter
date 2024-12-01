1a6997fca3add4c6b15416e111bbff423a021f5c
kdw <50583568+dong336@users.noreply.github.com>
2024-12-01 18:19:35 +0900

    2

--- lib/main.dart
+++ lib/main.dart
@@ -12,88 +12,88 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        home: Scaffold(
-        backgroundColor: color,
-        body: SafeArea(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              CircleAvatar(
-                radius: 50.0,
-                backgroundImage: AssetImage('images/me.jpg'),
-              ),
-              Text(
-                '김동운 dwk',
-                style: TextStyle(
-                  fontFamily: 'Pacifico',
-                  fontSize: 40.0,
-                  color: Colors.white,
-                  fontWeight: FontWeight.bold,
-                ),
-              ),
-              Text(
-                'Flutter Developer',
-                style: TextStyle(
-                  fontFamily: 'Source Sans 3',
-                  color: Colors.teal.shade100,
-                  fontSize: 20.0,
-                  letterSpacing: 2.5,
-                  fontWeight: FontWeight.bold,
-                ),
-              ),
-              SizedBox(
-                height: 20.0,
-                width: 250.0,
-                child: Divider(
-                  color: Colors.white,
-                )
-              ),
-              Card(
+      home: Scaffold(
+      backgroundColor: color,
+      body: SafeArea(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            CircleAvatar(
+              radius: 50.0,
+              backgroundImage: AssetImage('images/me.jpg'),
+            ),
+            Text(
+              '김동운 dwk',
+              style: TextStyle(
+                fontFamily: 'Pacifico',
+                fontSize: 40.0,
                 color: Colors.white,
-                margin: EdgeInsets.symmetric(
-                  vertical: 10.0,
-                  horizontal: 25.0,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            Text(
+              'Flutter Developer',
+              style: TextStyle(
+                fontFamily: 'Source Sans 3',
+                color: Colors.teal.shade100,
+                fontSize: 20.0,
+                letterSpacing: 2.5,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            SizedBox(
+              height: 20.0,
+              width: 250.0,
+              child: Divider(
+                color: Colors.white,
+              )
+            ),
+            Card(
+              color: Colors.white,
+              margin: EdgeInsets.symmetric(
+                vertical: 10.0,
+                horizontal: 25.0,
+              ),
+              child: ListTile(
+                leading: Icon(
+                  Icons.phone,
+                  color: color,
                 ),
-                child: ListTile(
-                  leading: Icon(
-                    Icons.phone,
+                title: Text(
+                  '010-6292-9619',
+                  style: TextStyle(
                     color: color,
-                  ),
-                  title: Text(
-                    '010-6292-9619',
-                    style: TextStyle(
-                      color: color,
-                      fontFamily: 'Source Sans 3',
-                      fontSize: 20.0,
-                    )
+                    fontFamily: 'Source Sans 3',
+                    fontSize: 20.0,
                   )
-                ),
-              ),
-              Card(
-                color: Colors.white,
-                margin: EdgeInsets.symmetric(
-                  vertical: 10.0,
-                  horizontal: 25.0,
-                ),
-                child: ListTile(
-                  leading: Icon(
-                    Icons.email,
-                    color: color,
-                  ),
-                  title: Text(
-                    'dong094724@gmail.com',
-                    style: TextStyle(
-                      fontSize: 20.0,
-                      color: color,
-                      fontFamily: 'Source Sans 3',
-                    ),
-                  ),
                 )
               ),
-            ],
-          )
-        ),
+            ),
+            Card(
+              color: Colors.white,
+              margin: EdgeInsets.symmetric(
+                vertical: 10.0,
+                horizontal: 25.0,
+              ),
+              child: ListTile(
+                leading: Icon(
+                  Icons.email,
+                  color: color,
+                ),
+                title: Text(
+                  'dong094724@gmail.com',
+                  style: TextStyle(
+                    fontSize: 20.0,
+                    color: color,
+                    fontFamily: 'Source Sans 3',
+                  ),
+                ),
+              )
+            ),
+          ],
+        )
       ),
+    ),
     );
   }
 }
